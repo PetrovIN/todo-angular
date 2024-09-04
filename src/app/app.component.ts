dee1e449177f28135f43b1e11ee0f0bb3f732cbc
petrovin <sakke9907@gmail.com>
2024-09-04 10:06:45 +0300

    started angular, add input and button to add changes to page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-angular';
+
+  filter: 'all' | 'active' | 'completed' = 'all'
+
+  allItems = [
+    { description: 'something', completed: true },
+    { description: 'need to do', completed: false }
+  ]
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      completed: false
+    });
+  }
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'completed' ? item.completed : !item.completed);
+  }
+ 
 }
