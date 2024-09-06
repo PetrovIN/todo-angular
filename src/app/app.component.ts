03abb485137d48721ffab00229948c33f2a18ca3
petrovin <sakke9907@gmail.com>
2024-09-06 14:33:06 +0300

    now can't add empty field to list, try to make clear completed button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
@@ -10,27 +11,40 @@ export class AppComponent {
 
   filter: 'all' | 'active' | 'completed' = 'all'
 
-  allItems = [
-    { description: 'something', completed: false },
-    { description: 'need to do', completed: false }
+  allItems: Item[] = [
+    // { description: 'something', completed: false },
+    // { description: 'need to do', completed: false }
   ]
 
-  addItem(description: string) {
-    this.allItems.unshift({
-      description,
-      completed: false
-    });
+  addItem(description: string): void {
+    if (description) {
+      this.allItems.unshift({
+        description,
+        completed: false
+      });
+    }
   }
 
-  remove(item: any) {
+  remove(item: any): void {
     this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+
+  removeAllCompleted(): void {
+    this.allItems.forEach(
+      item => {
+        if (item.completed) {
+          console.log('before ',this.allItems);
+          this.allItems.splice(this.allItems.indexOf(item), 1);
+          console.log('after ',this.allItems);
+      }
+    });
   }
 
   get items() {
     if (this.filter === 'all') {
       return this.allItems;
     }
-    return this.allItems.filter(item => this.filter === 'completed' ? item.completed : !item.completed);
+    return this.allItems.filter(item => this.filter === 'completed' ? item.completed : !item.completed)                   ;
   }
  
 }
